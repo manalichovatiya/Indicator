c884b89b38530245f4feba7e7a7e35b3e0dafaeb
manalichovatiya <115995019+manalichovatiya@users.noreply.github.com>
2023-03-23 20:14:31 +0530

    Add files via upload

---
+++ Indicator-3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    printf("Enter a string: ");
+ 	gets(str);
+    char *p = str;
+    printf("Length of the string: %d\n", strlen(p));
+
+    return 0;
+}
+
+//int main() {
+//  char str[100], *p;
+//  int len = 0;
+//  printf("Enter a string: ");
+//  gets(str);
+//  p = str;
+//  while (*p != '\0') {
+//    len++;
+//    p++;
+//  }
+//  printf("The length of the string is: %d\n", len);
+//
+//  return 0;
+//}
